ce760db0e786f4ca425be5cac100268e67379e4f
HemeshMahra <hemesh.mahra@techsuperiors.com>
2023-04-03 00:37:57 +0530

    update

---
+++ scrips.js
@@ -0,0 +1,9 @@
+function toggle(){
+    document.getElementById('phoneview').style.display="block"
+    document.getElementById('hidefa').style.display="none "
+}
+function toggletwo(){
+    document.getElementById('phoneview').style.display="none"
+    document.getElementById('hidefa').style.display="block"
+   
+}
